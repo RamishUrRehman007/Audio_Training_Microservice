dcf5ebbdab06bbcdb274ebc424b82768f7c27883
RamishUrRehman007 <ramish534@outlook.com>
2022-07-26 18:57:58 +0500

    Defined new dto AudioFile and ModelPrediction

--- audio_training_microservice/dto.py
+++ audio_training_microservice/dto.py
@@ -1,6 +1,11 @@
-from typing import List, Optional, Dict, Any
+from typing import List, Optional, Dict, Any, NewType
+from datetime import datetime
 
 from pydantic import BaseModel
+
+
+AudioFileID = NewType("AudioFileID", int)
+ModelPredictionID = NewType("ModelPredictionID", int)
 
 
 JSON = Dict[str, Any]
@@ -23,3 +28,21 @@ class Prediction(BaseModel):
     phrase: str
     time: int
     confidence: float
+
+
+class AudioFile(BaseModel):
+    id: AudioFileID
+    audio_file_name: str
+    audio_file_duration: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class ModelPrediction(BaseModel):
+    id: ModelPredictionID
+    audio_file_id: AudioFileID
+    confidence_utterance: str
+    confidence_time: int
+    confidence_confidence: float
+    created_at: datetime
+    updated_at: datetime
